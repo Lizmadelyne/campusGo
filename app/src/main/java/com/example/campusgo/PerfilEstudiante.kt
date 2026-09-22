54f20b3e21a522ea42ad754e8d0da5b10d63235a
Liz Madelyne <lizfajardop@gmail.com>
2026-09-22 08:17:41 -0500

    se subieron los dos activitys de santiago

---
+++ app/src/main/java/com/example/campusgo/PerfilEstudiante.kt
@@ -0,0 +1,20 @@
+package com.example.campusgo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PerfilEstudiante : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_perfil_estudiante)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
